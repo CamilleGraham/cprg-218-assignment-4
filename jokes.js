c7240691acb6760a8acb472c73f3517d6ebb9634
CamilleGraham <camillergraham@yahoo.ca>
2025-11-22 22:52:38 -0700

    function set up
    just added const for joke <p> and heading <h1>

--- jokes.js
+++ jokes.js
@@ -4,7 +4,16 @@ fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
         joke.textContent = data.joke;
     })
 
+function newJoke(catagory){
+fetch(`https://v2.jokeapi.dev/joke/${category}?safe-mode`)
+    .then(response => response.json() )
+    .then(data =>{
+        const jokeElement = document.getElementById("joke");
+        const headingElement = document.getElementById("heading");
+    })
+
 .catch(error =>{
     console.error ("error fetching joke", error);
-    joke.textContent = "No one is funny ALL the time!";
+    joke.textContent = "I can't think of a joke. No one is funny ALL the time!";
 })
+}
