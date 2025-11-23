eb107a81d0f84d0d51ce649a7eff9aa114694c2e
CamilleGraham <camillergraham@yahoo.ca>
2025-11-23 11:59:07 -0700

    trying Else if's
    not sure if these are right but i don't know how to check it yet so hooking up buttons next and we'll see

--- jokes.js
+++ jokes.js
@@ -5,15 +5,31 @@ fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
     })
 
 function newJoke(catagory){
-fetch(`https://v2.jokeapi.dev/joke/${category}?safe-mode`)
+    fetch(`https://v2.jokeapi.dev/joke/${category}?safe-mode`)
     .then(response => response.json() )
     .then(data =>{
         const jokeElement = document.getElementById("joke");
         const headingElement = document.getElementById("heading");
     })
 
-.catch(error =>{
+    if (data.type==="single"){
+        jokeElement.textContent = data.joke; 
+    }
+        else {
+        jokeElement.textContent = data.setup + "..." + data.delivery;
+        }
+
+    if (data.category==="Pun"){
+        headingElement.textContent = "A Random Pun!";
+    }
+        else {
+        headingElement.textContent = 'A ${category} Joke';
+        }
+
+    .catch(error => { 
     console.error ("error fetching joke", error);
-    joke.textContent = "I can't think of a joke. No one is funny ALL the time!";
+    jokeElement.textContent = "I can't think of a joke. No one is funny ALL the time!";
 })
+
 }
+
