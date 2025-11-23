243beb72b7bcff43c2c09979acd03f4f5e71ad8d
CamilleGraham <camillergraham@yahoo.ca>
2025-11-22 18:51:56 -0700

    minor updates
    just updated a few lines

--- jokes.js
+++ jokes.js
@@ -1,7 +1,10 @@
-fetch("https://v2.jokeapi.dev/joke/Any?blacklistFlags=nsfw,racist,sexist,explicit")
+fetch("https://v2.jokeapi.dev/joke/Any?safe-mode")
     .then(response => response.json() )
     .then(data =>{
         joke.textContent = data.joke;
-
     })
 
+.catch(error =>{
+    console.error ("error fetching joke", error);
+    joke.textContent = "No one is funny ALL the time!";
+})
